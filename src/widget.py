36165e2ed7e7eb4b1384525299b93d374880a280
MaxZ <maxzonoff@gmail.com>
2024-07-06 11:58:54 +0300

    отформатировал if

--- src/widget.py
+++ src/widget.py
@@ -7,10 +7,9 @@ def mask_account_card(type_number_account_card: str) -> str:
     str_alpha, str_number = type_number_account_card.rsplit(" ", 1)
 
     if len(str_number) == 16:
-
         return f"{str_alpha} {get_mask_card_number(str_number)}"
-    else:
-        return f"{str_alpha} {get_mask_account(str_number)}"
+
+    return f"{str_alpha} {get_mask_account(str_number)}"
 
 
 def get_date(date: str) -> str:

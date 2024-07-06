1c88d8fac7b936317b7f97129a11250e6e4083f5
MaxZ <maxzonoff@gmail.com>
2024-07-06 11:55:55 +0300

    изменил способ разделения типа и карты/счета

--- src/widget.py
+++ src/widget.py
@@ -4,13 +4,7 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(type_number_account_card: str) -> str:
     """Функция принимает тип и номер карты и счета и возвращает замаскированный номер"""
 
-    str_alpha = ""
-    str_number = ""
-    for i in type_number_account_card:
-        if i.isdigit():
-            str_number += i
-        else:
-            str_alpha += i
+    str_alpha, str_number = type_number_account_card.rsplit(" ", 1)
 
     if len(str_number) == 16:
 
@@ -21,6 +15,3 @@ def mask_account_card(type_number_account_card: str) -> str:
 
 def get_date(date: str) -> str:
     return ".".join(reversed(date[0:10].split("-")))
-
-
-print(get_date("2024-03-11T02:26:18.671407"))

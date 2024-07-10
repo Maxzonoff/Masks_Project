0894dddb77d037fc03844bc4a8d5e98142dcbe5b
MaxZ <maxzonoff@gmail.com>
2024-07-10 12:48:28 +0300

    написан код

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,15 @@
 def filter_by_state(list_dict: list, state="EXECUTED") -> list:
-    pass
+    """Функция сортрует словари в списках по значению ключа state."""
+    set_value = []
+    for val in list_dict:
+        if val["state"] == state:
+            set_value.append(val)
+
+    return set_value
 
 
 def sort_by_date(list_dict: list, date: str) -> list:
-    pass
+    """Функция сортировки дат по убыванию."""
+    list_dict.sort(key=lambda x: x[date], reverse=True)
+
+    return list_dict

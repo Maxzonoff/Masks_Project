933fed4737631275ae458bb5896c65e8c4197f8d
MaxZ <maxzonoff@gmail.com>
2024-07-10 13:59:31 +0300

    Revert "Feature/homework 10 1"

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,6 @@
 def filter_by_state(list_dict: list, state="EXECUTED") -> list:
-    """Функция сортирует словари в списках по значению ключа state."""
-    set_value = []
-    for val in list_dict:
-        if val["state"] == state:
-            set_value.append(val)
-
-    return set_value
+    pass
 
 
 def sort_by_date(list_dict: list, date: str) -> list:
-    """Функция сортировки дат по убыванию."""
-    list_dict.sort(key=lambda x: x[date], reverse=True)
-
-    return list_dict
+    pass

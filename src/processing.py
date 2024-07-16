db3649bee7f478d38e8980e3feb4a0c517108521
MaxZ <maxzonoff@gmail.com>
2024-07-17 00:37:58 +0300

    тесты masks, processing, widget

--- src/processing.py
+++ src/processing.py
@@ -6,13 +6,13 @@ def filter_by_state(
 
     result_list: list[dict[str, str | int]] = []
     for item in list_of_dicts:
-        if item["state"] == state:
+        if item.get("state") == state:
             result_list.append(item)
     return result_list
 
 
 def sort_by_date(list_of_dicts: list[dict[str, str | int]], ascending: bool = True) -> list[dict[str, str | int]]:
     """Функция сортировки дат по убыванию."""
-    list_of_dicts.sort(key=lambda x: x["date"], reverse= ascending)
+    list_of_dicts.sort(key=lambda x: x["date"], reverse=ascending)
 
     return list_of_dicts
---
+++ tests/test_processing.py
@@ -0,0 +1,49 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+@pytest.fixture()
+def list_dicts():
+    return [
+        {},
+        {"id": 41428829},
+        {"state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"state": "EXECUTED"},
+        {"state": "CANCELED"},
+        {"state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_filter_by_state_default_state(list_dicts):
+    assert filter_by_state(list_dicts) == [
+        {"state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"state": "EXECUTED"},
+    ]
+
+
+def test_filter_by_state_is_empty():
+    assert filter_by_state([]) == []
+
+
+def test_sort_by_date_is_empty():
+    assert sort_by_date([]) == []
+
+
+@pytest.fixture
+def test_list_dicts_date():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_sort_by_date_key_date(test_list_dicts_date, reverse=True):
+    assert sort_by_date(test_list_dicts_date, ascending=True) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]

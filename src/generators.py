056f5910a4dbba0431efb5d79458f15e7b46ec71
MaxZ <maxzonoff@gmail.com>
2024-08-06 20:28:24 +0300

    add generators + test

--- src/generators.py
+++ src/generators.py
@@ -1,24 +1,26 @@
 def filter_by_currency(transactions: list[dict], currency_code: str):
+    """Фильтрация транзакций по валюте."""
     for transaction in transactions:
-        transaction_currency_code = transaction['operationAmount']['currency']['code']
+        transaction_currency_code = transaction["operationAmount"]["currency"]["code"]
         if transaction_currency_code == currency_code:
             yield transaction
 
 
 def transaction_descriptions(transactions: list[dict]):
+    """Фильтрация по описанию транзакции."""
     for transaction in transactions:
-        yield transaction['description']
+        yield transaction["description"]
 
 
 def card_number_generator(start: int, stop: int):
     """Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты."""
     if start < 0 or start > 9999_9999_9999_9999:
-        raise ValueError('Старт должен быть в диапазоне от 0 до 9999_9999_9999_9999')
+        raise ValueError("Старт должен быть в диапазоне от 0 до 9999_9999_9999_9999")
     if stop < 0 or stop > 9999_9999_9999_9999:
-        raise ValueError('Стоп должен быть в диапазоне от 0 до 9999_9999_9999_9999')
+        raise ValueError("Стоп должен быть в диапазоне от 0 до 9999_9999_9999_9999")
     if start > stop:
-        raise ValueError('Старт не может быть больше стопа')
-    for i in range(start, stop+1):
+        raise ValueError("Старт не может быть больше стопа")
+    for i in range(start, stop + 1):
         s = str(i).zfill(16)
         list_s = [s[0:4], s[4:8], s[8:12], s[12:16]]
-        yield ' '.join(list_s)
+        yield " ".join(list_s)
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,20 +1,35 @@
 import pytest
+
 from src.generators import card_number_generator
 
 
-@pytest.mark.parametrize('start', [-1, 10 ** 17])
+# from .fixtures import transaction
+
+
+def test_filter_by_currency(transactions, 'USD'):
+    pass
+
+
+def test_transaction_descriptions():
+    pass
+
+
+@pytest.mark.parametrize("start", [-1, 10 ** 17])
 def test_card_number_generator_invalid_start(start):
     with pytest.raises(ValueError):
         next(card_number_generator(start, 9999_9999_9999_9999))
-# нужен тест на неправильны стоп и тест если старт больше чем стоп
+
+
+# нужен тест на неправильный стоп и тест если старт больше чем стоп
+
 
 def test_card_number_generator():
     c = card_number_generator(9998, 10003)
-    assert next(c) == '0000 0000 0000 9998'
-    assert next(c) == '0000 0000 0000 9999'
-    assert next(c) == '0000 0000 0001 0000'
-    assert next(c) == '0000 0000 0001 0001'
-    assert next(c) == '0000 0000 0001 0002'
-    assert next(c) == '0000 0000 0001 0003'
+    assert next(c) == "0000 0000 0000 9998"
+    assert next(c) == "0000 0000 0000 9999"
+    assert next(c) == "0000 0000 0001 0000"
+    assert next(c) == "0000 0000 0001 0001"
+    assert next(c) == "0000 0000 0001 0002"
+    assert next(c) == "0000 0000 0001 0003"
     with pytest.raises(StopIteration):
         next(c)

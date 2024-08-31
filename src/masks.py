40e753648c1e6d9e8f3a4ed27a85dbdaf7faa5ec
MaxZ <maxzonoff@gmail.com>
2024-08-31 14:13:48 +0300

    добавлено логирование

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,31 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("logs/log_masks.log", mode="w+")
+logger.addHandler(handler)
+formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+handler.setFormatter(formatter)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номер карты."""
     if len(card_number) < 16:
+        logger.error("Неверная длина карты")
         raise ValueError("Неверная длина карты")
 
-    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    card_number_mask = f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    logger.info("Успешно замаскировали карту")
+    return card_number_mask
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция, которая маскирует номер счета."""
 
     if len(account_number) < 20:
+        logger.error("Неверная длина счета")
         raise ValueError("Неверная длина счета")
 
-    return f"**{account_number[-4:]}"
+    account_number_mask = f"**{account_number[-4:]}"
+    logger.info("Успешно замаскировали счет")
+    return account_number_mask
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,29 @@
 import json
+import logging
+import typing
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("logs/log_utils.log", mode="w+")
+logger.addHandler(handler)
+formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+handler.setFormatter(formatter)
 
 
-def get_operations_info(file_path: str) -> list:
+def get_operations_info(file_path: str) -> list[typing.Any]:
     """Функция принимающая путь до файла и возвращающая python объект"""
     try:
         with open(file_path) as f:
             data_info = json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
+    except FileNotFoundError:
+        logger.error(f"Файл {file_path} не найден")
+        return []
+    except json.JSONDecodeError:
+        logger.error(f"Не верный формат json {file_path}")
         return []
 
     if not isinstance(data_info, list):
+        logger.warning(f"json должен содержать список, а содержит {type(data_info)}")
         return []
+    logger.info("Файл загружен")
     return data_info

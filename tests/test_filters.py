12186dafcf4ba73250579559b35ea8022df216ba
MaxZ <maxzonoff@gmail.com>
2024-09-18 00:51:20 +0300

    'add tests by filters'

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -2,5 +2,95 @@ from src.filters import filter_by_description, count_by_category
 
 
 def test_filter_by_description_empty():
-    assert filter_by_description(transaction, 'карт')
-    pass
+    assert filter_by_description([], "qwerty") == []
+
+
+def test_filter_by_description_search_ok(transaction):
+    assert filter_by_description(transaction, "Перевод организации") == [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+
+def test_filter_by_description_search_not_ok(transaction):
+    assert filter_by_description(transaction, "qwerty") != [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+
+def test_filter_by_description_search_ignore_case(transaction):
+    assert filter_by_description(transaction, "перевод организации") == [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+
+def test_count_by_category(transaction):
+    assert count_by_category(transaction, []) == {}
+
+
+def test_not_categories(transaction):
+    categories = ["qwerty"]
+    assert count_by_category(transaction, categories) == {}
+
+
+def test_empty_categories(transaction):
+    assert count_by_category(transaction, []) == {}
+
+
+def test_count_by_category_ok(transaction):
+    categories = ["Перевод организации", "Перевод со счета на счет", "Перевод с карты на карту"]
+    assert count_by_category(transaction, categories) == {
+        "Перевод организации": 2,
+        "Перевод со счета на счет": 2,
+        "Перевод с карты на карту": 1
+    }
